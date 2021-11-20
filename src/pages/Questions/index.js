98b3423a80d94ab0393ad99ee065c5cbc5ec2148
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-11-20 11:55:03 -0300

    create page questions

---
+++ src/pages/Questions/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Questions() {
+  return (
+    <div>
+      Questions
+    </div>
+  );
+}
+
+export default Questions;
